1d3d7b3e580c1b29be67bd6dfe3d10e1fad3f293
harshj3915 <hjain3915@gmail.com>
2025-04-02 17:12:13 +0530

    Add Google Analytics script to RootLayout for tracking page views

--- twai/app/layout.jsx
+++ twai/app/layout.jsx
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import SessionProviderWrapper from "../components/SP"
 import "./globals.css";
+import Script from 'next/script';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -21,9 +22,26 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics Script */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-6N4L55FL66`}
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-6N4L55FL66', {
+              page_path: window.location.pathname,
+            });
+          `}
+        </Script>
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <SessionProviderWrapper>
-         {children}         
+          {children}
         </SessionProviderWrapper>
 
       </body>
